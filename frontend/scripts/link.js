e19c822e945f1258d1821251495aec36c731630c
Encho Genchev <esgenchev@gmail.com>
2025-04-18 11:31:19 -0500

    Revert "updated"
    This reverts commit f0ab89548a20e5cadd9c4c44729048d4bd72f4ac.

--- frontend/scripts/link.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const profileLink = document.getElementById('profile-link');
-    const mainContent = document.getElementById('main-content');
-  
-    profileLink.addEventListener('click', (event) => {
-      event.preventDefault();
-      mainContent.innerHTML = '<p>Profile link clicked!</p>'; // Simple test
-    });
-  });
